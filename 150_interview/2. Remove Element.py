092f340c83069cd961c7ecd0f9d27d09d73d051e
Vinson Pham <cuongpham.iec@gmail.com>
2024-04-01 23:50:18 -0500

    update 2. algorithm

--- 150_interview/2. Remove Element.py
+++ 150_interview/2. Remove Element.py
@@ -49,6 +49,10 @@ Constraints:
 
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
-        for i in range(len(nums) - 1, -1, -1):
-            if nums[i] == val:
-                nums.pop(i)
+        k = 0
+        # Loop through very element in nums
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
